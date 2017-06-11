162d07c2f4a12165d9ee8ecde6cdc591d0b2b8dc
Ish <persoishtar@gmail.com>
2017-06-11 16:56:31 +0200

    Initial Commit

---
+++ BMP2PNG/MainForm.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace BMP2PNG
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Main_progressBar = new System.Windows.Forms.ProgressBar();
+            this.Main_checkbox = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(54, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Drop BMP images here ...";
+            // 
+            // Main_progressBar
+            // 
+            this.Main_progressBar.Location = new System.Drawing.Point(12, 81);
+            this.Main_progressBar.Name = "Main_progressBar";
+            this.Main_progressBar.Size = new System.Drawing.Size(213, 23);
+            this.Main_progressBar.TabIndex = 1;
+            // 
+            // Main_checkbox
+            // 
+            this.Main_checkbox.AutoSize = true;
+            this.Main_checkbox.Location = new System.Drawing.Point(12, 58);
+            this.Main_checkbox.Name = "Main_checkbox";
+            this.Main_checkbox.Size = new System.Drawing.Size(156, 17);
+            this.Main_checkbox.TabIndex = 2;
+            this.Main_checkbox.Text = "Delete BMP files afterwards";
+            this.Main_checkbox.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(237, 115);
+            this.Controls.Add(this.Main_checkbox);
+            this.Controls.Add(this.Main_progressBar);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Opacity = 0.8D;
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BMP2PNG";
+            this.TopMost = true;
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.MainForm_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.MainForm_DragEnter);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar Main_progressBar;
+        private System.Windows.Forms.CheckBox Main_checkbox;
+    }
+}
+
---
+++ BMP2PNG/MainForm.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BMP2PNG
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_DragDrop(object sender, DragEventArgs e)
+        {
+            // Get files dropped
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            // Prepare Progressbar
+            Main_progressBar.Style = ProgressBarStyle.Continuous;
+            Main_progressBar.Value = 0;
+            Main_progressBar.Maximum = files.Count();
+
+            // Check delete files
+            bool DeleteFiles = Main_checkbox.Checked;
+
+            foreach (string file in files)
+            {
+                // Check extension
+                if ((System.IO.Path.GetExtension(file).ToLower()) == ".bmp")
+                {
+                    try
+                    {
+                        Image img;
+                        using (System.IO.Stream stream = System.IO.File.OpenRead(file))
+                        {
+                            // Read image
+                            img = Image.FromStream(stream);
+
+                            // Convert image
+                            img.Save(System.IO.Path.ChangeExtension(file, ".png"), System.Drawing.Imaging.ImageFormat.Png);
+
+                            // Dispose ressources
+                            img.Dispose();
+                        }
+
+                        // Delete image if asked to
+                        if (DeleteFiles) System.IO.File.Delete(file);
+                        
+                    }
+                    catch(Exception ex)
+                    {
+                        MessageBox.Show("Error : " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);                 
+                    }
+
+                    Main_progressBar.Increment(1);
+
+                }
+            }
+
+        }
+
+        private void MainForm_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+        }
+
+    }
+}
